d2a5d41eae834420262c598fbfbe7124b035a309
thevimal98 <thevimal98@gmail.com>
2019-10-20 23:45:32 +0530

    Added client and server scripts

---
+++ dwac/client.py
@@ -0,0 +1,25 @@
+# Importing the socket module
+import socket
+
+# Create a socket object 
+sock = socket.socket()          
+  
+# Define the port on which you want to connect 
+port = 1013                
+  
+# Connect to the server on local computer 
+sock.connect(('127.0.0.1', port))
+
+# Sockname
+sockname = sock.getsockname()
+
+# Send sockname to the server
+sock.send(str(sockname[1]).encode('utf-8')) 
+  
+# Loop and close connection on condition
+while(True):    
+    # Receive data from the server 
+    data = sock.recv(1024).decode('utf-8')
+    print(data)
+    if(data=='Exit'):
+        break
---
+++ dwac/server.py
@@ -0,0 +1,44 @@
+# Importing the socket module
+import socket                
+  
+# Create a socket object 
+sock = socket.socket()          
+  
+# Reserving a port for socket binding
+port = 1013                
+  
+# Bind the port the socket
+sock.bind(('', port))         
+print("Socket bound to %s" %(port)) 
+  
+# Put the socket into listening mode 
+sock.listen(5)      
+print("Socket is currently listening")
+  
+# List of client socknames
+clients = []
+
+# Just to limit stuff
+n = 5
+
+
+while(n>0): 
+  
+   # Establish connection with client. 
+   client, addr = sock.accept()      
+   print('Got connection from', addr[0])
+
+   # Receive data from client
+   data = client.recv(1024).decode('utf-8')
+   print(data)
+
+   # Add client to client list
+   clients.append(client.getpeername()[1])
+
+   # Send some messages back to client
+   client.send('Hey'.encode('utf-8'))
+   client.send("Exit".encode('utf-8'))
+   
+   n-=1
+
+print(clients)
