f366acd2fecae30d4aae875e35691ad2f20986df
the-lost-explorer <parundekaramey@gmail.com>
2019-11-05 19:07:10 +0530

    Fixed testing devices

--- dwac/test.py
+++ dwac/test.py
@@ -6,21 +6,23 @@
     free_memory - RAM available in MB (variable)
     processor_speed - processor speed in MHz (variable)
 """
-d1 = {'id':'1','status':'1','battery':'85','free_memory':'870','process_speed':'512'}
-d2 = {'id':'2','status':'2','battery':'78','free_memory':'500','process_speed':'202'}
-d3 = {'id':'3','status':'3','battery':'56','free_memory':'432','process_speed':'256'}
-d4 = {'id':'4','status':'4','battery':'43','free_memory':'329','process_speed':'640'}
-d5 = {'id':'5','status':'5','battery':'56','free_memory':'123','process_speed':'156'}
-d6 = {'id':'6','status':'6','battery':'37','free_memory':'1024','process_speed':'270'}
-d7 = {'id':'7','status':'7','battery':'23','free_memory':'650','process_speed':'1024'}
-d8 = {'id':'8','status':'8','battery':'12','free_memory':'400','process_speed':'45'}
-d9 = {'id':'9','status':'9','battery':'5','free_memory':'260','process_speed':'56'}
-d10 = {'id':'10','status':'10','battery':'59','free_memory':'170','process_speed':'700'}
-d11 = {'id':'11','status':'11','battery':'29','free_memory':'127','process_speed':'314'}
-d12 = {'id':'12','status':'12','battery':'32','free_memory':'56','process_speed':'900'}
-d13 = {'id':'13','status':'13','battery':'92','free_memory':'150','process_speed':'750'}
-d14 = {'id':'14','status':'14','battery':'100','free_memory':'270','process_speed':'280'}
-d15 = {'id':'15','status':'15','battery':'69','free_memory':'50','process_speed':'340'}
+""" Sample devices
+"""
+d1 = {'id':'1','status':0,'battery':'85','free_memory':870,'process_speed':512}
+d2 = {'id':'2','status':1,'battery':'78','free_memory':500,'process_speed':202}
+d3 = {'id':'3','status':1,'battery':'56','free_memory':432,'process_speed':256}
+d4 = {'id':'4','status':0,'battery':'43','free_memory':329,'process_speed':640}
+d5 = {'id':'5','status':1,'battery':'56','free_memory':123,'process_speed':156}
+d6 = {'id':'6','status':0,'battery':'37','free_memory':1024,'process_speed':270}
+d7 = {'id':'7','status':1,'battery':'23','free_memory':650,'process_speed':1024}
+d8 = {'id':'8','status':1,'battery':'12','free_memory':400,'process_speed':45}
+d9 = {'id':'9','status':1,'battery':'5','free_memory':260,'process_speed':56}
+d10 = {'id':'10','status':0,'battery':'59','free_memory':170,'process_speed':700}
+d11 = {'id':'11','status':1,'battery':'29','free_memory':127,'process_speed':314}
+d12 = {'id':'12','status':1,'battery':'32','free_memory':56,'process_speed':900}
+d13 = {'id':'13','status':1,'battery':'92','free_memory':150,'process_speed':750}
+d14 = {'id':'14','status':1,'battery':'100','free_memory':270,'process_speed':280}
+d15 = {'id':'15','status':1,'battery':'69','free_memory':50,'process_speed':340}
 
 def prioritise_device(devices):
 
