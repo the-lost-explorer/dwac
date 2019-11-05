10ae6e921e04c305a1bd2b16bc7d705d739c4b59
the-lost-explorer <parundekaramey@gmail.com>
2019-11-05 18:06:36 +0530

    Added concurrent update function

--- dwac/node.py
+++ dwac/node.py
@@ -1,11 +1,23 @@
 # Import
 import socket, devices, json, time
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect(('localhost', 15558))
+from timer import RepeatedTimer
 
 current_device = json.loads(devices.device.get_dummy_vals())
 print(current_device,type(current_device))
+
+def update_device(current_device):
+    if(current_device['charging']==0):
+        current_device['battery'] -= 1
+        if(current_device['battery']<=20):
+            raise KeyboardInterrupt
+            time.sleep(2)
+    print(current_device)
+
+rt = RepeatedTimer(5, update_device, current_device )
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect(('localhost', 15558))
 
 try:
     '''
@@ -20,19 +32,14 @@ try:
     Main loop here
     '''
     while(True):
+        time.sleep(2)
+        print("Inside while")
         
-
-
-
-        # after whatever is done, wait for 2 seconds, then update
-        time.sleep(2.0)
-        if(current_device['charging']==0):
-            current_device['battery'] -= 1
-            if(current_device['battery']<=20):
-                raise KeyboardInterrupt
-        print(current_device)
 
 except KeyboardInterrupt:
     exit_message = "EXIT "+str(current_device['id'])
     server.send(exit_message.encode('utf-8'))
     server.close()
+    rt.stop()
+finally:
+    rt.stop()
